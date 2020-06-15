0917931ac97a79fc2c4040cae00cf0d00d7e3d0c
SerhiiRuban81 <serhii.ruban@ukr.net>
2020-06-15 20:43:28 +0300

    Добавлен класс Person

---
+++ StaffRecruitmentApp/Models/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaffRecruitmentApp.Models
+{
+    public class Person : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public int Id { get; set; }
+
+        public string Firstname { get; set; }
+
+        public string Surname { get; set; }
+
+        public int Age { get; set; }
+    }
+}
